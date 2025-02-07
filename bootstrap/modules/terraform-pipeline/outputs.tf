72d7f686c249ccc3d698c65fca561bed812c64df
gsistelos <gsistelos@proton.me>
2025-02-06 23:50:50 -0300

    trying to fix azure login

--- bootstrap/modules/terraform-pipeline/outputs.tf
+++ bootstrap/modules/terraform-pipeline/outputs.tf
@@ -1,9 +1,9 @@
 output "terraform_pipeline_credentials" {
   value = {
-    client_id       = azuread_service_principal.terraform_pipeline.client_id
-    client_secret   = azuread_service_principal_password.terraform_pipeline.value
-    subscription_id = data.azurerm_subscription.current.subscription_id
-    tenant_id       = data.azurerm_subscription.current.tenant_id
+    clientId       = azuread_service_principal.terraform_pipeline.client_id
+    clientSecret   = azuread_service_principal_password.terraform_pipeline.value
+    subscriptionId = data.azurerm_subscription.current.subscription_id
+    tenantId       = data.azurerm_subscription.current.tenant_id
   }
 
   sensitive = true
