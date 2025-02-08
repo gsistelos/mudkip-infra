4084bb01a6357c7828617e7169245889e7e41f85
gsistelos <gsistelos@proton.me>
2025-02-08 14:11:46 -0300

    remove check variables and readd subscription id

--- bootstrap/modules/tf-pipeline/outputs.tf
+++ bootstrap/modules/tf-pipeline/outputs.tf
@@ -1,8 +1,9 @@
 output "tf_pipeline_credentials" {
   value = {
-    ARM_CLIENT_ID     = azuread_service_principal.tf_pipeline.client_id
-    ARM_CLIENT_SECRET = azuread_service_principal_password.tf_pipeline.value
-    ARM_TENANT_ID     = data.azurerm_subscription.current.tenant_id
+    ARM_CLIENT_ID       = azuread_service_principal.tf_pipeline.client_id
+    ARM_CLIENT_SECRET   = azuread_service_principal_password.tf_pipeline.value
+    ARM_SUBSCRIPTION_ID = data.azurerm_subscription.current.subscription_id
+    ARM_TENANT_ID       = data.azurerm_subscription.current.tenant_id
   }
 
   sensitive = true
